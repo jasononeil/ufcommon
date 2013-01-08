435cb713f2dde695feab5c98e558ad577f3328ed
Jason O'Neil <jason.oneil@gmail.com>
2013-01-08 13:16:14 +0800

    Updated admin views

--- src/ufcommon/view/admin/AdminView.hx
+++ src/ufcommon/view/admin/AdminView.hx
@@ -1,16 +1,19 @@
 package ufcommon.view.admin;
 
+import dtx.widget.Widget;
 import dtx.widget.WidgetLoop;
 using Detox;
 
-class AdminView extends dtx.widget.Widget 
+class AdminView extends Widget {}
+
+class MigrationListView extends Widget 
 {
 	public var loop:WidgetLoop<String>;
 
 	public function new()
 	{
 		super();
-		loop = new WidgetLoop(AdminView_ModelRow, "modelName");
+		loop = new WidgetLoop(MigrationListView_ModelRow, "modelName");
 		modelTable.append(loop);
 	}
 }
