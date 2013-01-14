ce302bfd37747afd8b4771a1cf7ce657364e6640
Jason O'Neil <jason.oneil@gmail.com>
2013-01-14 11:49:05 +0800

    Major updating of all the views for admin stuff

--- src/ufcommon/view/admin/AdminView.hx
+++ src/ufcommon/view/admin/AdminView.hx
@@ -1,45 +1,5 @@
 package ufcommon.view.admin;
 
 import dtx.widget.Widget;
-import dtx.widget.WidgetLoop;
-import ufcommon.db.Migration.MigrationResults;
-using Detox;
 
 class AdminView extends Widget {}
-
-class MigrationListView extends Widget 
-{
-	public var runUp:WidgetLoop<String>;
-	public var runDown:WidgetLoop<String>;
-	public var alreadyRun:WidgetLoop<String>;
-
-	public function new()
-	{
-		super();
-
-		runUp = new WidgetLoop(MigrationListView_MigrationRowUp, "migration");
-		runDown = new WidgetLoop(MigrationListView_MigrationRowDown, "migration");
-		alreadyRun = new WidgetLoop(MigrationListView_MigrationRowDown, "migration");
-
-		migrationsToRunUpList.append(runUp);
-		migrationsToRunDownList.append(runDown);
-		migrationsAlreadyRunList.append(alreadyRun);
-	}
-
-	public function addResults(mr:MigrationResults)
-    {
-        var resultsWidget = new MigrationListView_Results();
-        resultsWidget.name = mr.name;
-        
-        for (r in mr.results)
-        {
-            var output = new MigrationListView_ResultsDisplay();
-            output.statement = r.statement;
-            output.result = r.result;
-            resultsWidget.output.append(output);
-        }
-
-        this.results.append(resultsWidget);
-        this.results.addClass("well");
-    }
-}
---
+++ src/ufcommon/view/admin/MigrationView.hx
@@ -0,0 +1,43 @@
+package ufcommon.view.admin;
+
+import dtx.widget.Widget;
+import dtx.widget.WidgetLoop;
+import ufcommon.db.Migration.MigrationResults;
+using Detox;
+
+class MigrationView extends Widget 
+{
+	public var runUp:WidgetLoop<String>;
+	public var runDown:WidgetLoop<String>;
+	public var alreadyRun:WidgetLoop<String>;
+
+	public function new()
+	{
+		super();
+
+		runUp = new WidgetLoop(MigrationView_MigrationRowUp, "migration");
+		runDown = new WidgetLoop(MigrationView_MigrationRowDown, "migration");
+		alreadyRun = new WidgetLoop(MigrationView_MigrationRowDown, "migration");
+
+		migrationsToRunUpList.append(runUp);
+		migrationsToRunDownList.append(runDown);
+		migrationsAlreadyRunList.append(alreadyRun);
+	}
+
+	public function addResults(mr:MigrationResults)
+    {
+        var resultsWidget = new MigrationView_Results();
+        resultsWidget.name = mr.name;
+        
+        for (r in mr.results)
+        {
+            var output = new MigrationView_ResultsDisplay();
+            output.statement = r.statement;
+            output.result = r.result;
+            resultsWidget.output.append(output);
+        }
+
+        this.results.append(resultsWidget);
+        this.results.addClass("well");
+    }
+}
