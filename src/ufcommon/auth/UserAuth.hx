8add0e35a6b7cbe2804db26abc1c7e5d1e5e1f12
Jason O'Neil <jason.oneil@gmail.com>
2013-01-22 01:46:54 +0800

    Added requireLogin, requirePermission, requirePermissions, getUser

--- src/ufcommon/auth/UserAuth.hx
+++ src/ufcommon/auth/UserAuth.hx
@@ -11,6 +11,48 @@ import ufcommon.auth.PermissionErrors;
 class UserAuth
 {
 	#if server
+		/** Require that a user is logged in.  Will throw DBUserAuthAdapter.NotLoggedIn if they are not. 
+
+		You can pass a custom message in the error if desired. */
+		public static function requireLogin(?msg:String)
+		{
+			if (isLoggedIn == false)
+			{
+				if (msg == null) msg = "You need to be logged in to do that...";
+				throw NotLoggedIn(msg);
+			}
+		}
+
+		/** Require that the user is authenticated and has a certain permission.  
+
+		Will throw DBUserAuthError, either NotLoggedIn or DoesNotHavePermission if they do not.
+
+		If the user does have permission, the code will continue to execute normally. 
+
+		You can pass a custom message in the error if you desire. */
+		public static function requirePermission(permission:EnumValue, ?msg:String)
+		{
+			requireLogin();
+			if (user.can(permission) == false)
+			{
+				if (msg == null) 
+				{
+					var permissionName = Type.enumConstructor(permission);
+					msg = 'You do not have the `$permissionName` permission required for this action.';
+					throw DoesNotHavePermission(msg);
+				}
+			}
+		}
+
+		/** Require that a user has several different permissions. Essentially calls requirePermission() on each. */
+		public static function requirePermissions(permissions:Iterable<EnumValue>, ?msg)
+		{
+			for (permission in permissions)
+			{
+				requirePermission(permission, msg);
+			}
+		}
+
 		/** Set to the number of seconds the session should last.  By default, value=0, which will end at the end of the session. */
 		public static var sessionLength:Int = 0;
 
@@ -34,6 +76,19 @@ class UserAuth
 			return _auth;
 		}
 
+		static var _user:User;
+		public static function getUser()
+		{
+			if (_user == null)
+			{
+				if (getAuth().hasIdentity())
+				{
+					_user = getAuth().getIdentity();
+				}
+			}
+			return _user;
+		}
+
 		public static function startSession(authAdapter:DBUserAuthAdapter)
 		{
 			var authResult = getAuth().authenticate(authAdapter);
@@ -48,6 +103,7 @@ class UserAuth
 		{
 			getAuth().clearIdentity();
 		}
+
 
 		public static var isLoggedIn(get,never):Bool;
 		static function get_isLoggedIn()
