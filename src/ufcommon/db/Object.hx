7154530dd121ab9296c3b08026db4303205c6555
Jason O'Neil <jason.oneil@gmail.com>
2013-03-29 12:54:43 +1100

    Added documentation comments, combined classes with conditional compilation
    Rather than having two separate classes, have one and just use conditional compilation
    for the different bits.

--- src/ufcommon/db/Object.hx
+++ src/ufcommon/db/Object.hx
@@ -6,15 +6,34 @@ package ufcommon.db;
 	import ufcommon.db.Types;
 #end
 
+/** Extended Database Object
+
+Builds on sys.db.Object, but adds: a default unique ID (unsigned Int), as well as created and modified timestamps.
+
+Also has methods to keep the timestamps up to date, and a generic "save" method when you're not sure if you need to insert or update.
+
+This class also uses conditional compilation so that the objects can exist on non-server targets that have no access to sys.db.*, on
+these platforms the objects can be created and exist, but have no access to save(), insert(), update() or delete().  
+
+We tell if it's a server platform by seeing checking for the #server define, so on your neko/php/cpp targets use `-D server`.
+*/
 #if server
 	@noTable
 	@:autoBuild(ufcommon.db.DBMacros.setupRelations())
-	class Object extends sys.db.Object
-	{
-		public var id:SUId;
-		public var created:SDateTime;
-		public var modified:SDateTime;
+#else
+	@:keepSub
+	@:rtti
+	@:autoBuild(ufcommon.db.DBMacros.setupRelations())
+#end 
+class Object #if server extends sys.db.Object #end
+{
+	public var id:SUId;
+	public var created:SDateTime;
+	public var modified:SDateTime;
 
+	#if server
+
+		/** Updates the "created" and "modified" timestamps, and then saves to the database. */
 		override public function insert()
 		{
 			this.created = Date.now();
@@ -22,12 +41,17 @@ package ufcommon.db;
 			super.insert();
 		}
 
+		/** Updates the "modified" timestamp, and then saves to the database. */
 		override public function update()
 		{
 			this.modified = Date.now();
 			super.update();
 		}
 		
+		/** Either updates or inserts the given record into the database, updating timestamps as necessary. 
+
+		If `id` is null, then it needs to be inserted.  If `id` already exists, try to update first.  If that
+		throws an error, it means that it is not inserted yet, so then insert it. */
 		public function save()
 		{
 			if (id == null)
@@ -48,28 +72,18 @@ package ufcommon.db;
 				}
 			}
 		}
-	}
-#else 
-	@:keepSub
-	@:rtti
-	@:autoBuild(ufcommon.db.DBMacros.setupRelations())
-	class Object
-	{
-		public var id:SId;
-		public var created:SDateTime;
-		public var modified:SDateTime;
+	
+	#else
 
+		// Empty versions of these functions for the client.
 		public function new() {}
-		
-		// Currently not implemented, but we should...
-		
-		public function save()   { throw "Not implemented yet."; } // Remoting call (or saves to local storage and wait for a sync()?) 
-		public function delete() { throw "Not implemented yet."; } // Remoting call (or saves to local storage and wait for a sync()?) 
-		public function insert() { throw "Not implemented yet."; } // Remoting call (or saves to local storage and wait for a sync()?)
-		public function update() { throw "Not implemented yet."; } // Remoting call (or saves to local storage and wait for a sync()?)
-		
-	}
-#end
+		public function save()   { throw "Cannot save ufcommon.db.Object from the client."; } // Remoting call (or saves to local storage and wait for a sync()?) 
+		public function delete() { throw "Cannot delete ufcommon.db.Object from the client."; } // Remoting call (or saves to local storage and wait for a sync()?) 
+		public function insert() { throw "Cannot insert ufcommon.db.Object from the client."; } // Remoting call (or saves to local storage and wait for a sync()?)
+		public function update() { throw "Cannot update ufcommon.db.Object from the client."; } // Remoting call (or saves to local storage and wait for a sync()?)
+	
+	#end
+}
 
 typedef BelongsTo<T> = T;
 typedef HasMany<T> = Iterable<T>;
