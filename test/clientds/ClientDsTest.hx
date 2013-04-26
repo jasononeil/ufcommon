eca084052db5d6fd510ba8bb85543b7e665d5f3f
Jason O'Neil <jason.oneil@gmail.com>
2013-04-26 14:36:12 +0800

    Add unit testing stuff.  Not that it's working yet.

---
+++ test/clientds/ClientDsTest.hx
@@ -0,0 +1,46 @@
+package clientds;
+
+import massive.munit.util.Timer;
+import massive.munit.Assert;
+import massive.munit.async.AsyncFactory;
+import clientds.ClientDs;
+
+/**
+* Auto generated MassiveUnit Test Class  for clientds.ClientDs 
+*/
+class ClientDsTest 
+{
+	var instance:ClientDs; 
+	
+	public function new() 
+	{
+		
+	}
+	
+	@BeforeClass
+	public function beforeClass():Void
+	{
+	}
+	
+	@AfterClass
+	public function afterClass():Void
+	{
+	}
+	
+	@Before
+	public function setup():Void
+	{
+	}
+	
+	@After
+	public function tearDown():Void
+	{
+	}
+	
+	
+	@Test
+	public function testExample():Void
+	{
+		Assert.isTrue(false);
+	}
+}
